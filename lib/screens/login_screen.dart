818acdc1e1bcef6bf3c6f2b00f163a4116147e95
purity anyango <puritybanyango@gmail.com>
2026-07-14 19:41:03 +0300

    added the login page

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -10,13 +10,116 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _loginFormKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   bool _hidePassword = true;
-  
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(24),
+          child: Form(
+            key: _loginFormKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Image.asset("assets/images/image.png", width: 160, height: 160),
+
+                SizedBox(height: 16),
+
+                Text('Login',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+                ),
+
+                SizedBox(height: 16),
+
+                TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                prefixIcon: Icon(Icons.email_outlined),
+                labelText: 'Email',
+                border: OutlineInputBorder()
+                ),
+
+                validator: (value) {
+                  if(value == null || value.isEmpty) {
+                    return 'Enter your email';
+                  }
+                  if(!value.contains('@')) {
+                    return 'Enter a valid email';
+                  }
+                  return null;
+                },
+                keyboardType: TextInputType.emailAddress,
+                ),
+
+                SizedBox(height: 16),
+
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    prefixIcon: Icon(Icons.lock_outline),
+                    border: OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                      setState(() {
+                      _hidePassword = !_hidePassword;
+                     });
+                      },
+                    icon: Icon(_hidePassword ? Icons.visibility_outlined : Icons.visibility_off_outlined))
+                   ),
+                  validator: (value){
+                    if(value == null || value.isEmpty){
+                      return 'Enter your password';
+                    }
+                    return null;
+                  },
+                  obscureText: _hidePassword,
+                ),
+
+                SizedBox(height: 24),
+                
+
+                ElevatedButton(
+                  onPressed: (){
+                    if(_loginFormKey.currentState!.validate()){
+                      //Perform login & navigate to home screen
+                    }
+                  }, 
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepPurple,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    )
+                  ),
+
+                  child: Text("Login",
+                  style:TextStyle(color: Colors.white)),
+
+                
+               
+                ),
+                SizedBox(height: 16),
+                
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Not yet Registered"),
+
+                    TextButton(onPressed: (){}, child: Text("Sign Up"))
+                  ],
+
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
-
